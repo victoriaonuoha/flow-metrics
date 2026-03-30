63a3bc39065a2c6fa8d435c4698faec76cad0e6f
victoriaonuoha <onuohavictoria27august@gmail.com>
2026-03-30 16:36:43 +0100

    gh

---
+++ src/app/customers/page.jsx
@@ -0,0 +1,8 @@
+export default function CustomersPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Customers</h1>
+      <p className="text-gray-600">List of customers will go here.</p>
+    </div>
+  );
+}
---
+++ src/app/plans/page.jsx
@@ -0,0 +1,8 @@
+export default function PlansPage() {
+  return (
+        <div>
+        <h1 className="text-2xl font-bold mb-4">Plans</h1>
+        <p className="text-gray-600">List of plans will go here.</p>
+        </div>
+  );
+}
